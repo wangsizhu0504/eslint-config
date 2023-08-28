36959f8e21a9b8fe3a36ef8be1840fb95d7d2d1b
Kriszu <wangsizhu0504@outlook.com>
2023-08-28 10:39:38 +0800

    fix: eslint-plugin-import

--- packages/eslint-plugin/src/configs/rules/eslint-vue3.ts
+++ packages/eslint-plugin/src/configs/rules/eslint-vue3.ts
@@ -4,7 +4,7 @@ export default defineRules({
   'vue/prefer-import-from-vue': 'off',
   'vue/require-prop-types': 'off',
   // reactivity transform
-  'vue/no-setup-props-destructure': 'off',
+  'vue/no-setup-props-reactivity-loss': 'off',
   'vue/define-macros-order': ['error', {
     order: [
       'defineOptions',
