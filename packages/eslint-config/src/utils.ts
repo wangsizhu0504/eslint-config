cc5956db17f908bffcad1f3d2f9b4c265f70f862
Kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2023-12-24 22:47:32 +0800

    fix: sync

--- packages/eslint-config/src/utils.ts
+++ packages/eslint-config/src/utils.ts
@@ -54,3 +54,24 @@ export async function ensurePackages(packages: string[]) {
   if (result)
     await import('@antfu/install-pkg').then(i => i.installPackage(nonExistingPackages, { dev: true }))
 }
+
+export const parserPlain = {
+  meta: {
+    name: 'parser-plain',
+  },
+  parseForESLint: (code: string) => ({
+    ast: {
+      body: [],
+      comments: [],
+      loc: { end: code.length, start: 0 },
+      range: [0, code.length],
+      tokens: [],
+      type: 'Program',
+    },
+    scopeManager: null,
+    services: { isPlain: true },
+    visitorKeys: {
+      Program: [],
+    },
+  }),
+}
