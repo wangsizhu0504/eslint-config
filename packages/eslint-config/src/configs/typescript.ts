a9a6dcc03fdb74ebe419becc626500ba15b28802
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-07-15 14:03:45 +0800

    feat(typescript): add 'ts/promise-function-async' to type aware rule

--- packages/eslint-config/src/configs/typescript.ts
+++ packages/eslint-config/src/configs/typescript.ts
@@ -56,6 +56,7 @@ export async function typescript(
     'ts/no-unsafe-call': 'warn',
     'ts/no-unsafe-member-access': 'error',
     'ts/no-unsafe-return': 'error',
+    'ts/promise-function-async': 'error',
     'ts/restrict-plus-operands': 'error',
     'ts/restrict-template-expressions': 'error',
     'ts/return-await': 'error',
