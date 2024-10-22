cda8f3f928e790f91b1d59266aeb78b3747f2e3f
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-10-22 10:29:00 +0800

    fix: check packages existence in local scope

--- packages/eslint-config/src/utils.ts
+++ packages/eslint-config/src/utils.ts
@@ -1,7 +1,11 @@
 import process from 'node:process'
 import { isPackageExists } from 'local-pkg'
 import type { Awaitable, TypedFlatConfigItem } from './types'
+import { fileURLToPath } from 'node:url'
 
+
+const scopeUrl = fileURLToPath(new URL('.', import.meta.url))
+const isCwdInScope = isPackageExists('@kriszu/eslint-config')
 export const parserPlain = {
   meta: {
     name: 'parser-plain',
@@ -107,11 +111,15 @@ export async function interopDefault<T>(m: Awaitable<T>): Promise<T extends { de
   return (resolved as any).default || resolved
 }
 
+export function isPackageInScope(name: string): boolean {
+  return isPackageExists(name, { paths: [scopeUrl] })
+}
+
 export async function ensurePackages(packages: (string | undefined)[]): Promise<void> {
-  if (process.env.CI || process.stdout.isTTY === false)
+  if (process.env.CI || process.stdout.isTTY === false || isCwdInScope === false)
     return
 
-  const nonExistingPackages = packages.filter(i => i && !isPackageExists(i)) as string[]
+  const nonExistingPackages = packages.filter(i => i && !isPackageInScope(i)) as string[]
   if (nonExistingPackages.length === 0)
     return
 
