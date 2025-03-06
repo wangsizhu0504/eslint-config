6a6670f6bc0f8d3a3703d576acebb3b4a7c701e7
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-03-06 17:44:36 +0800

    fix: remove declaration  module

--- packages/eslint-config/src/declaration.d.ts
+++ packages/eslint-config/src/declaration.d.ts
@@ -1,10 +1,5 @@
-declare module '@eslint-react/eslint-plugin';
 declare module 'eslint-plugin-react-hooks';
 declare module 'eslint-plugin-react-refresh';
 declare module '@eslint-community/eslint-plugin-eslint-comments';
 declare module 'eslint-plugin-import-x';
-declare module 'eslint-plugin-n';
-declare module 'eslint-plugin-unicorn';
-declare module 'eslint-plugin-unused-imports';
-declare module 'eslint-plugin-perfectionist';
 declare module 'eslint-plugin-no-only-tests';
