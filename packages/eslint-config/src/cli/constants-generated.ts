d7108c8d99761f22488d0e66fe5debf46c27f952
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-03-24 16:47:09 +0800

    fix: version

--- packages/eslint-config/src/cli/constants-generated.ts
+++ packages/eslint-config/src/cli/constants-generated.ts
@@ -1,7 +1,7 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^1.34.1",
-  "@unocss/eslint-plugin": "^66.1.0-beta.5",
-  "eslint": "^9.22.0",
+  "@eslint-react/eslint-plugin": "^1.37.3",
+  "@unocss/eslint-plugin": "^66.0.0",
+  "eslint": "^9.23.0",
   "eslint-plugin-format": "^1.0.1",
   "eslint-plugin-react-hooks": "^5.2.0",
   "eslint-plugin-react-refresh": "^0.4.19"
