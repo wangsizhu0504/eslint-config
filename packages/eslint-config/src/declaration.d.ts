3d61055c841290655af1ac447fde0c71203cbe95
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-03-24 16:42:14 +0800

    chore: eslint-plugin-react-hooks declaration

--- packages/eslint-config/src/declaration.d.ts
+++ packages/eslint-config/src/declaration.d.ts
@@ -1,5 +1,3 @@
-declare module 'eslint-plugin-react-hooks';
-declare module 'eslint-plugin-react-refresh';
 declare module '@eslint-community/eslint-plugin-eslint-comments';
 declare module 'eslint-plugin-import-x';
 declare module 'eslint-plugin-no-only-tests';
