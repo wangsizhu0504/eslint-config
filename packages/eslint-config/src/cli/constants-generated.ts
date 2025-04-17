74effaabca60ad55d063eb3a777579c3081baf44
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-04-17 16:06:05 +0800

    feat: sync the recommended rules of react plugins

--- packages/eslint-config/src/cli/constants-generated.ts
+++ packages/eslint-config/src/cli/constants-generated.ts
@@ -1,8 +1,8 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^1.37.3",
-  "@unocss/eslint-plugin": "^66.0.0",
-  "eslint": "^9.23.0",
-  "eslint-plugin-format": "^1.0.1",
-  "eslint-plugin-react-hooks": "^5.2.0",
-  "eslint-plugin-react-refresh": "^0.4.19"
+  '@eslint-react/eslint-plugin': '^1.38.4',
+  '@unocss/eslint-plugin': '^66.0.0',
+  'eslint': '^9.23.0',
+  'eslint-plugin-format': '^1.0.1',
+  'eslint-plugin-react-hooks': '^5.2.0',
+  'eslint-plugin-react-refresh': '^0.4.19',
 }
--- packages/eslint-config/src/configs/react.ts
+++ packages/eslint-config/src/configs/react.ts
@@ -81,7 +81,6 @@ export async function react(
       },
     },
     {
-      name: 'kriszu/react/rules',
       files,
       languageOptions: {
         parserOptions: {
@@ -91,8 +90,11 @@ export async function react(
         },
         sourceType: 'module',
       },
+      name: 'kriszu/react/rules',
       rules: {
         // recommended rules from eslint-plugin-react-x https://eslint-react.xyz/docs/rules/overview#core-rules
+        'react/jsx-no-duplicate-props': 'warn',
+        'react/jsx-uses-vars': 'warn',
         'react/no-access-state-in-setstate': 'error',
         'react/no-array-index-key': 'warn',
         'react/no-children-count': 'warn',
@@ -109,7 +111,6 @@ export async function react(
         'react/no-create-ref': 'error',
         'react/no-default-props': 'error',
         'react/no-direct-mutation-state': 'error',
-        'react/no-duplicate-jsx-props': 'warn',
         'react/no-duplicate-key': 'warn',
         'react/no-forward-ref': 'warn',
         'react/no-implicit-key': 'warn',
@@ -130,7 +131,6 @@ export async function react(
         'react/no-unused-state': 'warn',
         'react/no-use-context': 'warn',
         'react/no-useless-forward-ref': 'warn',
-        'react/use-jsx-vars': 'warn',
 
         // recommended rules from eslint-plugin-react-dom https://eslint-react.xyz/docs/rules/overview#dom-rules
         'react-dom/no-dangerously-set-innerhtml': 'warn',
@@ -152,10 +152,11 @@ export async function react(
         // recommended rules eslint-plugin-react-hooks https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks/src/rules
         'react-hooks/exhaustive-deps': 'warn',
         'react-hooks/rules-of-hooks': 'error',
-        // react
+
         // recommended rules from eslint-plugin-react-hooks-extra https://eslint-react.xyz/docs/rules/overview#hooks-extra-rules
         'react-hooks-extra/no-direct-set-state-in-use-effect': 'warn',
         'react-hooks-extra/no-unnecessary-use-prefix': 'warn',
+        'react-hooks-extra/prefer-use-state-lazy-initialization': 'warn',
 
         // recommended rules from eslint-plugin-react-web-api https://eslint-react.xyz/docs/rules/overview#web-api-rules
         'react-web-api/no-leaked-event-listener': 'warn',
@@ -205,9 +206,9 @@ export async function react(
     },
     ...isTypeAware
       ? [{
-          name: 'kriszu/react/type-aware-rules',
           files: filesTypeAware,
           ignores: ignoresTypeAware,
+          name: 'kriszu/react/type-aware-rules',
           rules: {
             ...typeAwareRules,
           },
