53783cb950c2a95ae1d53891088547d025c00198
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-04-18 14:04:57 +0800

    chore: update deps

--- packages/eslint-config/src/cli/constants-generated.ts
+++ packages/eslint-config/src/cli/constants-generated.ts
@@ -1,7 +1,7 @@
 export const versionsMap = {
-  '@eslint-react/eslint-plugin': '^1.38.4',
+  '@eslint-react/eslint-plugin': '^1.48.3',
   '@unocss/eslint-plugin': '^66.0.0',
-  'eslint': '^9.23.0',
+  'eslint': '^9.24.0',
   'eslint-plugin-format': '^1.0.1',
   'eslint-plugin-react-hooks': '^5.2.0',
   'eslint-plugin-react-refresh': '^0.4.19',
