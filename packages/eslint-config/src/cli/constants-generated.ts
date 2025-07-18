fa559e188ef0fdd38688d83c4a3100fea1f23286
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-07-18 13:52:57 +0800

    chore: update deps

--- packages/eslint-config/src/cli/constants-generated.ts
+++ packages/eslint-config/src/cli/constants-generated.ts
@@ -1,10 +1,10 @@
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^1.48.3",
-  "@unocss/eslint-plugin": "^66.0.0",
-  "eslint": "^9.24.0",
+  "@eslint-react/eslint-plugin": "^1.49.0",
+  "@unocss/eslint-plugin": "^66.1.0",
+  "eslint": "^9.26.0",
   "eslint-plugin-format": "^1.0.1",
   "eslint-plugin-react-hooks": "^5.2.0",
-  "eslint-plugin-react-refresh": "^0.4.19"
+  "eslint-plugin-react-refresh": "^0.4.20"
 }
