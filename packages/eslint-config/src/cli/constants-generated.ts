a4f39493d228cdcf37145cee0af3885da7bb0d77
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2026-02-10 18:18:57 +0800

    feat: upgrade eslint-plugin-react-refresh, eslint-plugin-regexp

--- packages/eslint-config/src/cli/constants-generated.ts
+++ packages/eslint-config/src/cli/constants-generated.ts
@@ -1,11 +1,11 @@
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^2.11.0",
-  "@next/eslint-plugin-next": "^16.1.4",
+  "@eslint-react/eslint-plugin": "^2.12.2",
+  "@next/eslint-plugin-next": "^16.1.6",
   "@unocss/eslint-plugin": "^66.6.0",
   "eslint": "^9.39.2",
-  "eslint-plugin-format": "^1.3.1",
+  "eslint-plugin-format": "^1.4.0",
   "eslint-plugin-react-hooks": "^7.0.1",
-  "eslint-plugin-react-refresh": "^0.4.26"
+  "eslint-plugin-react-refresh": "^0.5.0"
 }
